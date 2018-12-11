8af6aa59dd9acde792dc5fcad666d0b65bf6a450
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2018-12-11 17:09:14 -0200

    Unit test for criteria querying by an embedded entity property.

--- grails-datastore-gorm-test/src/test/groovy/org/grails/datastore/gorm/EmbeddedAssociationSpec.groovy
+++ grails-datastore-gorm-test/src/test/groovy/org/grails/datastore/gorm/EmbeddedAssociationSpec.groovy
@@ -27,6 +27,28 @@ class EmbeddedAssociationSpec extends GormDatastoreSpec {
             i.bio.birthday.date == now
     }
 
+    void "Test criteria query by embedded entities"() {
+        given:
+            def i = new Individual(name:"Bob", address: new Address(postCode:"30483"), bio: new Bio(birthday: new Birthday(now)))
+            def j = new Individual(name:"Doug", address: new Address(postCode:"80050"), bio: new Bio(birthday: new Birthday(now)))
+
+            i.save(flush:true)
+            j.save(flush:true)
+            session.clear()
+
+        when:
+            def criteria = Individual.createCriteria()
+            def results = criteria.list {
+                address {
+                    eq('postCode', '80050')
+                }
+            }
+
+        then:
+            results.size() == 1
+            results[0].name == 'Doug'
+    }
+
     @Override
     List getDomainClasses() {
         [Individual, Address]
